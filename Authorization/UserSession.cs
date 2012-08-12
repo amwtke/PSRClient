502f06e6acc2215d0d56f347a096599c1c24582d
xiaojin.pku@gmail.com <xiaojin.pku@gmail.com@0d6648ed-654a-3043-3256-06a5477b8ba5>
2012-08-12 03:00:47 +0000

    1

---
+++ Authorization/UserSession.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace APP
+{
+    public class UserSession
+    {
+        static User _loginUser = null;
+        public static void IniSession(User u)
+        {
+            _loginUser = u;
+        }
+        public static User LoginUser
+        {
+            get
+            {
+                if (_loginUser != null)
+                    return _loginUser;
+                //throw new Exception("Session丢失");
+                return null;
+            }
+        }
+    }
+}
