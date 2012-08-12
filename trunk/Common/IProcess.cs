124e031ab5a3c74de3f2686b4eb1ccdb2df8c36d
xiaojin.pku@gmail.com <xiaojin.pku@gmail.com@0d6648ed-654a-3043-3256-06a5477b8ba5>
2012-08-12 03:15:45 +0000

    1

---
+++ trunk/Common/IProcess.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace APP
+{
+    public delegate void ProcessLineHandler(string linestr);
+    public interface IProcessText
+    {
+        ProcessLineHandler datahandler
+        {
+            get;
+        }
+    }
+}
