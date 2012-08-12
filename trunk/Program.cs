6144590c4452dbcc0b6e64402f0de8d603ae43d6
xiaojin.pku@gmail.com <xiaojin.pku@gmail.com@0d6648ed-654a-3043-3256-06a5477b8ba5>
2012-08-12 03:22:40 +0000

    1

---
+++ trunk/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace APP
+{
+    static class Program
+    {
+        /// <summary>
+        /// 应用程序的主入口点。
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            LoginForm login = new LoginForm();
+            DialogResult result = login.ShowDialog();
+            if (login.CancelLogin)
+            {
+                return;
+            }
+            if (UserSession.LoginUser == null)
+            {
+
+            }
+            else
+            {
+                Application.Run(new MainForm());
+            }
+        }
+    }
+}
