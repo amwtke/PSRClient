f12b79b0aa42d4dd7c843c5d2bb513ed0029d351
xiaojin.pku@gmail.com <xiaojin.pku@gmail.com@0d6648ed-654a-3043-3256-06a5477b8ba5>
2012-08-12 03:21:10 +0000

    1

---
+++ Tree/NodesActions/AdminTab.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace APP
+{
+    public enum ControlAuthor
+    {
+        Enable,
+        Disable,
+        Invisible,
+        Show,
+        EnableAndInvisible,
+        EnableAndShow,
+        DisableAndInvisible,
+        DisableAndShow,
+    }
+    public class AdminTab : ITreeNodeAction
+    {
+        string _nodeName;
+        string ITreeNodeAction.getNoteName
+        {
+            get
+            {
+                return _nodeName;
+            }
+            set
+            {
+                _nodeName = value;
+            }
+        }
+
+        void ITreeNodeAction.ShowForm()
+        {
+            TreeNodeHelper.ShowFormInPanel<AdminForm>();
+        }
+    }
+}
