27c35cd9ca9b73c37625c8f9dfce4927bb4914f2
xiaojin.pku@gmail.com <xiaojin.pku@gmail.com@0d6648ed-654a-3043-3256-06a5477b8ba5>
2012-08-13 13:46:48 +0000

    version 1

---
+++ Tabs/ImportUserAndStatusForm.Designer.cs
@@ -0,0 +1,123 @@
+﻿namespace APP
+{
+    partial class ImportUserAndStatusForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button_ok = new System.Windows.Forms.Button();
+            this.button_Cancel = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // button_ok
+            // 
+            this.button_ok.DialogResult = System.Windows.Forms.DialogResult.OK;
+            this.button_ok.Location = new System.Drawing.Point(62, 154);
+            this.button_ok.Name = "button_ok";
+            this.button_ok.Size = new System.Drawing.Size(75, 23);
+            this.button_ok.TabIndex = 0;
+            this.button_ok.Text = "确定";
+            this.button_ok.UseVisualStyleBackColor = true;
+            this.button_ok.Click += new System.EventHandler(this.button_ok_Click);
+            // 
+            // button_Cancel
+            // 
+            this.button_Cancel.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.button_Cancel.Location = new System.Drawing.Point(155, 154);
+            this.button_Cancel.Name = "button_Cancel";
+            this.button_Cancel.Size = new System.Drawing.Size(75, 23);
+            this.button_Cancel.TabIndex = 1;
+            this.button_Cancel.Text = "取消";
+            this.button_Cancel.UseVisualStyleBackColor = true;
+            this.button_Cancel.Click += new System.EventHandler(this.button_Cancel_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(130, 29);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(155, 21);
+            this.textBox1.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(35, 32);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(89, 12);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "导入记录用户：";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(35, 79);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(89, 12);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "导入记录状态：";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(130, 79);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(155, 20);
+            this.comboBox1.TabIndex = 4;
+            // 
+            // ImportUserAndStatusForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(304, 189);
+            this.ControlBox = false;
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button_Cancel);
+            this.Controls.Add(this.button_ok);
+            this.Name = "ImportUserAndStatusForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "导入数据";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button_ok;
+        private System.Windows.Forms.Button button_Cancel;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox comboBox1;
+    }
+}
---
+++ Tabs/SelectAgainstFactsForm.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace APP
+{
+    public partial class SelectAgainstFactsForm : Form
+    {
+        Tab1_AddAffectForm _innerForm = null;
+        TextBox _innerTB = null;
+        SortedList<int, string> _temp = new SortedList<int, string>();
+
+        public static int OpenCount = 0;
+        public SelectAgainstFactsForm()
+        {
+            InitializeComponent();
+        }
+
+        public SelectAgainstFactsForm(Tab1_AddAffectForm form,TextBox _textBox)
+        {
+            InitializeComponent();
+            _innerForm = form;
+            _innerTB = _textBox;
+
+            foreach (int i in _innerForm._unAgainstFactNo.Keys)
+            {
+                _temp.Add(i, "    " + _innerForm.GetTextOfPanelByIndex(i));
+                //checkedListBox1.Items.Add(i.ToString() + "    " + _innerForm.GetTextOfPanelByIndex(i), false);
+            }
+
+            if (_textBox.Text != null && _textBox.Text.Length > 0)
+            {
+                string[] myselflist = _textBox.Text.Split(new char[] { ',' });
+                if (myselflist != null && myselflist.Length > 0)
+                {
+                    foreach (string s in myselflist)
+                    {
+                        _temp.Add(int.Parse(s), "    " + _innerForm.GetTextOfPanelByIndex(int.Parse(s)));
+                        //checkedListBox1.Items.Add(s.ToString() + "    " + _innerForm.GetTextOfPanelByIndex(int.Parse(s)), false);
+                    }
+                }
+            }
+
+            foreach (KeyValuePair<int, string> pair in _temp)
+            {
+                checkedListBox1.Items.Add(pair.Key + pair.Value, false);
+            }
+
+            OpenCount++;
+        }
+
+        private void buttonOK_Click(object sender, EventArgs e)
+        {
+            if (checkedListBox1.CheckedItems.Count > 0)
+            {
+                if (_innerForm != null)
+                {
+                    List<int> tempList = new List<int>();
+                    for (int i = 0; i < checkedListBox1.CheckedItems.Count; i++)
+                    {
+                        int temp = int.Parse(checkedListBox1.CheckedItems[i].ToString().Trim().Split(new char[] { ' ' })[0]);
+                        tempList.Add(temp);
+                        _temp.Remove(temp);
+                    }
+                    _innerForm._unAgainstFactNo = _temp;
+                    _innerForm.AgainstStr = _innerForm.FormSelectFactString(tempList);
+                    _innerTB.Text = _innerForm.AgainstStr;
+                    _innerTB.Refresh();
+                }
+            }
+            this.Close();
+        }
+
+        private void SelectAgainstFactsForm_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            OpenCount--;
+        }
+
+        private void buttonCancel_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void checkedListBox1_Click(object sender, EventArgs e)
+        {
+            if(checkedListBox1.SelectedIndex>=0)
+                checkedListBox1.SetItemChecked(checkedListBox1.SelectedIndex, true);
+        }
+    }
+}
