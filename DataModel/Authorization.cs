1c3d9dcc0d4516f6a8bcd1c2eabeba2fcf0b8953
xiaojin.pku@gmail.com <xiaojin.pku@gmail.com@0d6648ed-654a-3043-3256-06a5477b8ba5>
2012-08-13 13:42:18 +0000

    version 1

---
+++ DataModel/Authorization.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace APP
+{
+    public class Authorization
+    {
+        public static bool IsControlVisiable(string parentControlName,string controlNmae,string checkPointName, string authParent)
+        {
+            if (!UserHelper.IsUserHaveAuth(checkPointName, authParent, UserSession.LoginUser.Roles[0]))
+            {
+                return UserHelper.DoesUserGotSpecialAuth(parentControlName, controlNmae);
+            }
+            return true;
+        }
+
+        public static bool IsControlVisiable(Control parent, Control control, string checkPointName, string authParent)
+        {
+            if (parent == null)
+            {
+                return IsControlVisiable(null, control.Name, checkPointName, authParent);
+
+            }
+            else
+                return IsControlVisiable(parent.Name, control.Name, checkPointName, authParent);
+        }
+    }
+}
---
+++ DataModel/User.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace APP
+{
+    [DB4OClass("System\\User.yap")]
+    public class User
+    {
+        string _stuffNo;
+        [Key("_Name")]
+        string _Name;
+        string _pingying;
+        string _password;
+        string _telephone;
+        string _email;
+        List<string> _roles;
+        bool _active;
+        string _facility;
+        string _danwei;
+        string _suoxie;
+
+        public string SuoXie
+        {
+            get { return _suoxie; }
+            set { _suoxie = value; }
+        }
+
+        public string DanWei
+        {
+            get { return _danwei; }
+            set { _danwei = value; }
+        }
+
+        public string Facility
+        {
+            get { return _facility; }
+            set { _facility = value; }
+        }
+        public string StuffNo
+        {
+            get { return _stuffNo; }
+            set { _stuffNo = value; }
+        }
+
+        public string Name
+        {
+            get { return _Name; }
+            set { _Name = value; }
+        }
+        public string PingYing
+        {
+            get { return _pingying; }
+            set { _pingying = value; }
+        }
+        public string Password
+        {
+            get { return _password; }
+            set { _password = value; }
+        }
+        public string Email
+        {
+            get { return _email; }
+            set { _email = value; }
+        }
+        public string Telephone
+        {
+            get { return _telephone; }
+            set { _telephone = value; }
+        }
+
+        public List<string> Roles
+        {
+            get { return _roles; }
+            set { _roles = value; }
+        }
+
+        public string Role
+        {
+            get
+            {
+                if (_roles != null)
+                {
+                    return _roles[0];
+                }
+                return null;
+            }
+        }
+
+        public bool Active
+        {
+            get { return _active; }
+            set { _active = value; }
+        }
+    }
+}
