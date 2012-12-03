a8e8431c4024fc9ae260f079e5b6960082ef1367
xiaojin.pku@gmail.com <xiaojin.pku@gmail.com@0d6648ed-654a-3043-3256-06a5477b8ba5>
2012-12-03 08:14:43 +0000

    使lefttree的配置文件与app.exe创建时间相匹配。不然不能使用。 现在的做法：在lefttree。yap文件中加入了app。exe的创建时间，在登陆的时候判断app的创建时间是否与lefttree一致，不然不能登陆。 也就是，要使用lefttree 必须有相匹配的app。

--- Authorization/LoginForm.cs
+++ Authorization/LoginForm.cs
@@ -6,6 +6,8 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
+using Db4objects.Db4o;
+using Db4objects.Db4o.Query;
 
 namespace APP
 {
@@ -23,6 +25,8 @@ namespace APP
         }
         private void button_OK_Click(object sender, EventArgs e)
         {
+            bool _checkFileAsso = true;
+
             this.CancelLogin = false;
             if (textBox_username.Text == "rinpo")
             {
@@ -32,26 +36,39 @@ namespace APP
                 u.Roles.Add("admin");
                 u.SuoXie = "adm";
                 u.DanWei = DanWeiManager.DanWeiList[0];
-                UserSession.IniSession(u); 
+                UserSession.IniSession(u);
+                _checkFileAsso = false;
                 this.Close();
             }else 
             if (!string.IsNullOrEmpty(textBox_username.Text))
             {
-                User u = UserHelper.GetUser(textBox_username.Text);
-                if (u != null && u.Password == textBox_password.Text)
+                if (textBox_username.Text == "admin")
+                    _checkFileAsso = false;
+
+                //检查文件绑定
+                if (_checkFileAsso && !CheckTimeIfMatch())
                 {
-                    UserSession.IniSession(u);
-                    this.Close();
-                }
-                else if(u!=null)
-                {
-                    textBox_password.Text = "";
-                    lb_message.Text = "密码错误！";
+                    MessageBox.Show("授权文件绑定失败！请到管理员处拷贝最新的App.exe以及系统配置文件！", "错误", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    button_OK.Enabled = false;
                 }
                 else
                 {
-                    textBox_password.Text = "";
-                    lb_message.Text = "用户不存在！";
+                    User u = UserHelper.GetUser(textBox_username.Text);
+                    if (u != null && u.Password == textBox_password.Text)
+                    {
+                        UserSession.IniSession(u);
+                        this.Close();
+                    }
+                    else if (u != null)
+                    {
+                        textBox_password.Text = "";
+                        lb_message.Text = "密码错误！";
+                    }
+                    else
+                    {
+                        textBox_password.Text = "";
+                        lb_message.Text = "用户不存在！";
+                    }
                 }
             }
             else
@@ -79,6 +96,67 @@ namespace APP
             {
                 button_OK.PerformClick();
             }
+        }
+
+        private DateTime? GetTreeDateTime<Root>() where Root : ITreeNodeRoot, new()
+        {
+            IObjectContainer _db;
+            Root _root;
+            TreeInforAttribute[] _atts = DBHelper.GetTreeInfoAttribute(typeof(Root));
+            if (_atts != null && _atts.Length >= 0)
+                _db = DBHelper.InitDB4O(_atts[0].DBName, typeof(Root));
+            else
+                throw new Exception("找不到DB！" + typeof(Root) + "没有TreeInfor属性？");
+
+            Root example = new Root();
+            example.RootValue = "admin";//user name
+            try
+            {
+                Root[] re = DBHelper.FindByExample<Root>(_db, example);
+                if (re != null && re.Length > 0)
+                {
+                    _root = re[0];
+                    if (_root is AuthorizationTree)
+                    {
+                        AuthorizationTree _temp = _root as AuthorizationTree;
+                        return _temp.AppCreationTime;
+                    }
+                    else if (_root is RootNode)
+                    {
+                        RootNode _temp = _root as RootNode;
+                        return _temp.AppCreationTime;
+                    }
+                    else
+                        return null;
+                }
+                return null;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                _db.Close();
+            }
+        }
+
+        private DateTime GetAppDateTime()
+        {
+            System.IO.FileInfo _fileInfo = new System.IO.FileInfo(Application.ExecutablePath);
+            return _fileInfo.CreationTime;
+        }
+
+        private bool CheckTimeIfMatch()
+        {
+            DateTime? _leftTreeTime = GetTreeDateTime<RootNode>();
+            if (_leftTreeTime == null) return false;
+            else
+            {
+                if (_leftTreeTime == GetAppDateTime())
+                    return true;
+            }
+            return false;
         }
     }
 }
